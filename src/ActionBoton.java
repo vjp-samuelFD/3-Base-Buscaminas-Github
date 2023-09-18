603563563e23ec594709d473b9c22c55b08cf923
2 DAW Samuel Fernandez Diaz <sfernandezd08@informatica.iesvalledeljerteplasencia.es>
2023-09-18 18:50:50 +0200

    h

--- src/ActionBoton.java
+++ src/ActionBoton.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
  * De alguna manera tendrá que poder acceder a la ventana principal.
  * Se puede lograr pasando en el constructor la referencia a la ventana.
  * Recuerda que desde la ventana, se puede acceder a la variable de tipo ControlJuego
- * @author jesusredondogarcia
+ * @author Samuel Fernández Díaz
  **
  */
 public class ActionBoton implements ActionListener{
